e60dbd83754dad0d0eeb426d76e3462e5aa0809d
Erdem <erdem_kirsehir_40@hotmail.com>
2019-08-20 17:39:10 +0200

    hiding the content

---
+++ stemwijzer.js
@@ -0,0 +1,12 @@
+var content = document.getElementById('content');
+var statements = document.getElementById('statements');
+var backBtn = document.getElementById('backButton');
+
+function Content(){
+	statements.style.display = 'none';
+	backBtn.style.display = 'none';
+	content.style.display = 'block';
+	
+	
+}
+
